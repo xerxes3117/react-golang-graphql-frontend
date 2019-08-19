b3d22dbf4f9dceb3b3ce297f7ca10a6cf1b47513
nikzayn <nikhilvaidyar1997@gmail.com>
2019-08-19 12:23:50 +0530

    React Setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>React - Book my Show</div>
+)
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
